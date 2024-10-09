cc8888d9f928d6cdc9eb4feddfc2e9de84ed6c2e
Eddy R. López Olivera <lopezoliveraeddy@gmail.com>
2024-10-09 05:54:28 -0600

    Se agrega el frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+import ProvidersList from './components/ProvidersList';
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <ProvidersList />
+    </div>
+  );
+}
+
+export default App;
+
